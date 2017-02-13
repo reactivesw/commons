a8a54119d79fbac767fce28de102b35b606323cc
umasuo <liuquan89@gmail.com>
2017-02-13 18:13:48 +0800

    add localized string & money & localized string serializer

---
+++ src/main/java/io/reactivesw/model/Money.java
@@ -0,0 +1,23 @@
+package io.reactivesw.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Davis on 16/11/16.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Money {
+  /**
+   * The Currency code.
+   */
+  private String currencyCode;
+
+  /**
+   * The Cent amount.
+   */
+  private Integer centAmount;
+}
